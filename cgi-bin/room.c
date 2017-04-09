95f65e5130d23ed11b4bdfa511f83aec398ea154
Jacky <jackymayonaise@gmail.com>
2017-04-09 03:42:33 -0400

    Fixed room cgi

--- cgi-bin/room.c
+++ cgi-bin/room.c
@@ -8,17 +8,17 @@ int main(void){
     int a = 0;
     int n;
     n = atoi(getenv("CONTENT_LENGTH"));
-
-    while((buffer = getchar()) != EOF && a < n){
-        if (a < 200){
-            if (buffer!='+') 
-                string[a] = buffer;
-            else  //also need to check for percent and ampersand
-                string[a]=' ';
-            a++;
-        }
-    }
-    string[a] = '\0';
+    // fgets(string,n,stdin);
+    // while((buffer = getchar()) != EOF && a < n){
+    //     if (a < 200){
+    //         if (buffer!='+') 
+    //             string[a] = buffer;
+    //         else  //also need to check for percent and ampersand
+    //             string[a]=' ';
+    //         a++;
+    //     }
+    // }
+    // string[a] = '\0';
 
     printf("%s%c%c\n","ContentType:text/html;charset=iso-8859-1",13,10);
     printf("%s", string);
