8526962e31b88c830b719ed60d3a53d3b2274c49
Jacky <jackymayonaise@gmail.com>
2017-04-09 13:22:44 -0400

    Check if the parser works.

--- cgi-bin/room.c
+++ cgi-bin/room.c
@@ -4,24 +4,23 @@
 int main(void){
     char string[200];
     char buffer;
-
+    FILE *file;
     int a = 0;
     int n;
     n = atoi(getenv("CONTENT_LENGTH"));
-     fgets(string,n,stdin);
-    // while((buffer = getchar()) != EOF && a < n){
-    //     if (a < 200){
-    //         if (buffer!='+') 
-    //             string[a] = buffer;
-    //         else  //also need to check for percent and ampersand
-    //             string[a]=' ';
-    //         a++;
-    //     }
-    // }
-    // string[a] = '\0';
+    while((buffer = getchar()) != EOF && a < n){
+        if (a < 200){
+            if (buffer!='+') 
+                string[a] = buffer;
+            else  //also need to check for percent and ampersand
+                string[a]=' ';
+            a++;
+        }
+    }
+    string[a] = '\0';
 
     printf("%s%c%c\n","ContentType:text/html;charset=iso-8859-1",13,10);
     printf("%s", string);
-
+    
     return 0;
 };
