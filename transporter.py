2e265f7bda93970d91810fcba185d4c987076e24
Jacky <jackymayonaise@gmail.com>
2017-04-10 10:20:42 -0400

    CSV working

--- transporter.py
+++ transporter.py
@@ -1,20 +1,21 @@
-import urlparsse
+#import urlparse
 import sys
 import csv
 
-with open("resource.csv", 'r') as my_file:
-    reader = csv.reader(my_file, delimiter='\t')
+with open("resources.csv", 'r') as my_file:
+    reader = csv.reader(my_file, delimiter=',')
     my_list = list(reader)
     print(my_list)
 
-url = ""
-parsed = urlparse.urlparse(url)
+print(my_list[0][2])
+#url = ""
+#parsed = urlparse.urlparse(url)
 
-def transporter("", parsed):
-
-
-def main(argv):
+#def transporter("", parsed):
 
 
-if __name__ == "__main__":
-	main(sys.argv)
+#def main(argv):
+
+
+#if __name__ == "__main__":
+#	main(sys.argv)
