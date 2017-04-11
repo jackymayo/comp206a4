8575d672dc68f1885b17898e519b39807a0f9fce
Jacky <jackymayonaise@gmail.com>
2017-04-10 21:48:58 -0400

    Test out the python in cgi-bin

--- cgi-bin/transporter.py
+++ cgi-bin/transporter.py
@@ -3,14 +3,19 @@ import csv
 import cgi
 import requests
 # Dictionary ["url":.."Inventory"]
-form = cgi.FieldStorage()
+# form = cgi.FieldStorage()
 
-print(form.getvalue("inventory"))
+# Create pseudo form for testing purpose
+form = ["URL": "http://cs.mcgill.ca/~jma229/cgi-bin/room.cgi", "inventory":"1,0"]
+
+player = form["inventory"].split(",")
+if (player[0] == "1"):
+	# Maybe read from a dead.html template?
+	print("You just killed yourself. 🤔")
 
 with open("resources.csv", "r") as my_file:
     reader = csv.reader(my_file, delimiter=',')
     my_list = list(reader)
-    #print(my_list)
 my_file.close()
 print(my_list[0])
 #If room isn't busy successssss
@@ -22,10 +27,4 @@ if (my_list[0][2] == "0"):
 	with open('resources.csv','w') as opened_file:
 		writer = csv.writer(opened_file)
 		writer.writerow([x for x in my_list[0]])
-		
-else:
-	#urlInv = form.values()
-	if form.has_key("url"):
-		theUrl = form["url"]
-		r = requests.post(theUrl, data = {'command':'Refresh'})
 
